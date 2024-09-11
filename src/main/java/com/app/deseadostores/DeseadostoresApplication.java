d4c86ff8a7c34a220e96db7391a5fd251bd4e09c
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-09-11 11:19:33 -0300

    Se agrega proyecto al repositorio

---
+++ src/main/java/com/app/deseadostores/DeseadostoresApplication.java
@@ -0,0 +1,13 @@
+package com.app.deseadostores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeseadostoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeseadostoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/deseadostores/DeseadostoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.deseadostores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeseadostoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
